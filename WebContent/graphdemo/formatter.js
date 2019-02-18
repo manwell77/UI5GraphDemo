7e2d9d70f7a7646f41aee5f29b45efa09a02f401
Manuel Bellet <manwell77@gmail.com>
2019-02-18 09:12:37 +0100

    Key

--- WebContent/graphdemo/formatter.js
+++ WebContent/graphdemo/formatter.js
@@ -8,7 +8,7 @@ sap.ui.define([], function () {
 			
 			if ((sHouseNo!=null) && (sStreet!=null) && (sZip!=null) && (sCity!=null) && (sRegion!=null) && (sCountry!=null)) {	
 				
-				var sFrame="<iframe width='100%' height='100%' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBZmGvj3AIzVqCX_GueyePYPBzTFXKfb0g&zoom=17&q="
+				var sFrame="<iframe width='100%' height='100%' src='https://www.google.com/maps/embed/v1/place?key=<maps_api_key>&zoom=17&q="
 					+ jQuery.sap.encodeURL(sHouseNo + " " + sStreet + ", " + sZip + " " + sCity + ", " + sRegion + ", " + sCountry) +		
 					"'/>";
 				
